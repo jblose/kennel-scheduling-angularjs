8feb124ecb2dab24e3246a9f338700e01b84397c
jblose <jamblose-github@yahoo.com>
2013-12-05 23:38:46 -0500

    Addition of datetimepicker and it's prerequisites. Implementation of vacancy search.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ui.utils','myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['ui.bootstrap.datetimepicker','ui.utils','myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/schedule', {templateUrl: 'partials/schedule.html', controller: 'ScheduleCtrl'});
-        $routeProvider.when('/reservation', {templateUrl: 'partials/reservation.html', controller: 'ReservationCtrl'})
+        $routeProvider.when('/reservationreservationreservationreservationreservation', {templateUrl: 'partials/reservation.html', controller: 'ReservationCtrl'})
         $routeProvider.when('/clientman/:action', {templateUrl: 'partials/clientman.html', controller: 'ClientManCtrl'})
         $routeProvider.when('/dogman', {templateUrl: 'partials/dogman.html', controller: 'DogManCtrl'})
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
