9a01e0aec0136dc471cbf5aa121bb4c9b62d9bbf
Jamison Blose <jamison.blose@wolterskluwer.com>
2013-12-10 17:33:26 -0500

    Scratch SQL file was unsaved at time of last commit.

--- db/scratch.sql
+++ db/scratch.sql
@@ -20,10 +20,10 @@ order by c.last_name,c.first_name
 
 
 
-select c.id, concat(c.last_name,', ',c.first_name, ' - ', group_concat( d.name separator ', ')) as full_name  
-from rsak.client c
-join rsak.client_dog_x cdx on (c.id = cdx.client_id)
-join rsak.dog d on (cdx.dog_id = d.id)
-where c.id > 0 
-group by c.id
-order by concat(c.last_name,', ',c.first_name) ;
+	select c.id, concat(c.last_name,', ',c.first_name, ' - ', group_concat( d.name separator ', ')) as full_name  
+	from rsak.client c
+	join rsak.client_dog_x cdx on (c.id = cdx.client_id)
+	join rsak.dog d on (cdx.dog_id = d.id)
+	where c.id > 0 
+	group by c.id
+	order by concat(c.last_name,', ',c.first_name);
