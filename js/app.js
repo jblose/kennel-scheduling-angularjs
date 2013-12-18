8f430d066598076e624c083b8d14e0b0429358aa
Jamison Blose <jamison.blose@wolterskluwer.com>
2013-12-18 14:51:27 -0500

    Updated to Angular 1.2.5

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ui.bootstrap.datetimepicker','ui.utils','myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['ngRoute','ui.bootstrap.datetimepicker','ui.utils','myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/schedule', {templateUrl: 'partials/schedule.html', controller: 'ScheduleCtrl'});
+        $routeProvider.when('/vacancy', {templateUrl: 'partials/vacsearch.html', controller: 'VacSearchCtrl'});
         $routeProvider.when('/reservation', {templateUrl: 'partials/reservation.html', controller: 'ReservationCtrl'});
         $routeProvider.when('/reservation/:resid', {templateUrl: 'partials/resview.html', controller: 'ResViewCtrl'});
         $routeProvider.when('/clientman/:action', {templateUrl: 'partials/clientman.html', controller: 'ClientManCtrl'});
